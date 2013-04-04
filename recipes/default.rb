94c3bd3737042f767627de10a1e7e9a624345f65
James Francis <tcotav@gmail.com>
2013-04-04 16:54:16 -0700

    errant end

--- recipes/default.rb
+++ recipes/default.rb
@@ -30,23 +30,12 @@ directory node['drupal']['windows']['path'] do
   action :create
 end
 
+#
+# TODO - find a way to clean up/fix the unzip'd path/directory
+#
 windows_zipfile node['drupal']['windows']['path'] do
   source distzipfile
   action :unzip
   not_if {::File.exists?(::File.join(node['drupal']['windows']['path'],'index.php'))}
 end
 
-
-
-
-#    # define the command
-#    command "#{node['drupal']['drush']['dir']}/drush -y dl drupal-#{node['drupal']['version']} --destination=#{File.dirname(node['drupal']['dir'])} --drupal-project-rename=#{File.basename(node['drupal']['dir'])} && \
-#    #{node['drupal']['drush']['dir']}/drush -y site-install -r #{node['drupal']['dir']} --account-name=#{node['drupal']['site']['admin']} --account-pass=#{node['drupal']['site']['pass']} --site-name=#{node['drupal']['site']['name']} \
-#    --db-url=mysql://#{node['drupal']['db']['user']}:'#{node['drupal']['db']['password']}'@localhost/#{node['drupal']['db']['database']}"
-#    not_if "#{node['drupal']['drush']['dir']}/drush -r #{node['drupal']['dir']} status | grep #{node['drupal']['version']}"
-#
-
-    # ref: http://drupal.org/documentation/install/windows
-
-
-end
