c1ed935a695b93b027c32c1ff1701b3b2fb75d2e
James Francis <tcotav@gmail.com>
2013-04-04 16:33:29 -0700

    fixed typo with vcruntime

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends "windows" 
-depends "vcruntime-cookbook"
+depends "vcruntime"
 depends "php-windows-cookbook"
 depends "apache2-windows-cookbook"
 #need sql server driver for php here
