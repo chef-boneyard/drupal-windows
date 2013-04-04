2f8154d0b9c966f1a9af15e6db142b76c26594fd
James Francis <tcotav@gmail.com>
2013-04-04 16:21:47 -0700

    y

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,9 @@ license          'All rights reserved'
 description      'Installs/Configures drupal-windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "windows" 
+depends "vcruntime-cookbook"
+depends "php-windows-cookbook"
+depends "apache2-windows-cookbook"
+#need sql server driver for php here
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,51 @@
 # Cookbook Name:: drupal-windows
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Opscode, Inc
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
+
+
+distfilename = "drupal-latest.zip"
+distzipfile = ::File.join(Chef::Config[:file_cache_path],distfilename)
+
+remote_file distzipfile do
+  source node['drupal']['windows']['source']
+  checksum node['drupal']['windows']['checksum']
+end
+
+directory node['drupal']['windows']['path'] do
+  action :create
+end
+
+windows_zipfile node['drupal']['windows']['path'] do
+  source distzipfile
+  action :unzip
+  not_if {::File.exists?(::File.join(node['drupal']['windows']['path'],'index.php'))}
+end
+
+
+
+
+#    # define the command
+#    command "#{node['drupal']['drush']['dir']}/drush -y dl drupal-#{node['drupal']['version']} --destination=#{File.dirname(node['drupal']['dir'])} --drupal-project-rename=#{File.basename(node['drupal']['dir'])} && \
+#    #{node['drupal']['drush']['dir']}/drush -y site-install -r #{node['drupal']['dir']} --account-name=#{node['drupal']['site']['admin']} --account-pass=#{node['drupal']['site']['pass']} --site-name=#{node['drupal']['site']['name']} \
+#    --db-url=mysql://#{node['drupal']['db']['user']}:'#{node['drupal']['db']['password']}'@localhost/#{node['drupal']['db']['database']}"
+#    not_if "#{node['drupal']['drush']['dir']}/drush -r #{node['drupal']['dir']} status | grep #{node['drupal']['version']}"
+#
+
+    # ref: http://drupal.org/documentation/install/windows
+
+
+end
