4f5bf7bbebfeb55211a23b70a7fd6328dd2c1981
James Francis <tcotav@gmail.com>
2013-04-11 10:50:08 -0700

    push drupal initial files
    Change-Id: I4c733a7b260f8725325c947cbf7d75f3639d9f03

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,17 +1,31 @@
-# local files
+default['drupal']['php']['install_path']="C:\\php"
+
 default['drupal']['windows']['source']['url'] = 'http://www.acquia.com/sites/default/files/downloads/acquia-drupal/7.x/acquia-drupal-7.21.20.5959.zip'
 default['drupal']['windows']['source']['checksum'] = '6dcb3a765e804f646908f99bd6cd45906959bd8908030c0256c1608de6b21cff'
-default['drupal']['windows']['path'] = 'c:\\drupal'
+default['drupal']['windows']['path'] = "C:\\Apache2\\htdocs"
+#default['drupal']['windows']['path'] = "#{node['apache2']['windows']['path']}\\htdocs"
 
 # azure php plugin  -- required by storage
 #default['drupal']['windows']['azure_php_sdk']['source'] = "d"
 #default['drupal']['windows']['azure_php_sdk']['checksum'] = "d"
 
+default['drupal']['windows']['modules']['path'] = "C:\\Apache2\\htdocs\\modules"
+
 # azure storage plugin
 default['drupal']['windows']['azure_storage']['source'] = 'http://ftp.drupal.org/files/projects/azure-7.x-1.0-rc1.zip'
 default['drupal']['windows']['azure_storage']['checksum'] = '292bf01bbade9c7b85996d602a8509aecdfe288f623948d96dd70a3a41a54e0d'
 
+
 # azure ACS plugin
 default['drupal']['windows']['azure_acs']['source'] = 'http://ftp.drupal.org/files/projects/azure_acs-7.x-1.0-rc1.zip'
 default['drupal']['windows']['azure_acs']['checksum'] = '71ff64922510bd40658844b8256178208ae9f243484b1225b29585b0c45ddee9'
+
+# ms sql server drivers for php
+default['drupal']['windows']['sqlserv-driver']['source'] = 'http://download.microsoft.com/download/C/D/B/CDB0A3BB-600E-42ED-8D5E-E4630C905371/SQLSRV30.EXE'
+default['drupal']['windows']['sqlserv-driver']['checksum'] = '6db35194c4e98f647cf8194f99904a55b3e21fd99acdf31bf789070a2b28202c'
+default['drupal']['windows']['database']['sqlserver']['driver']='php_sqlsrv_54_ts.dll'
+
+# sql server plugin for drupal
+default['drupal']['windows']['sqlserv-plugin']['source'] = 'http://ftp.drupal.org/files/projects/sqlsrv-7.x-1.2.zip'
+default['drupal']['windows']['sqlserv-plugin']['checksum'] = '4fc7c5ad7ff23cd7cf63f4df4bcfe4f66343fa44875b2f1be9e97840c410a604'
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -20,11 +20,18 @@
 include_recipe 'windows'
 include_recipe 'apache2-windows'
 include_recipe 'php-windows'
-require 'digest'
 
-sourcepath=::File.join(Chef::Config[:file_cache_path], "drupal")
-distfilename = "drupal-latest.zip"
-distzipfile = ::File.join(sourcepath, distfilename)
+
+###########################################################
+# TODO
+# conditional install of all azure bits (acs, blob storage)
+# as well as support for setup of both mysql and sqlserver
+###########################################################
+
+###########################################################
+# drupal install
+sourcepath="#{Chef::Config[:file_cache_path]}/drupal"
+distzipfile = "#{sourcepath}/drupal-latest.zip"
 
 # source directory where we land and unroll our zip
 directory sourcepath do
@@ -41,50 +48,28 @@ end
 remote_file distzipfile do
   source node['drupal']['windows']['source']['url']
   checksum node['drupal']['windows']['source']['checksum']
-  notifies :unzip, "windows_zipfile[#{sourcepath}]"
+  notifies :unzip, "windows_zipfile[#{sourcepath}]", :immediately
 end
 
 #source_index_file=::File.join(sourcepath,'index.php')
 windows_zipfile sourcepath do
   action :nothing
   source distzipfile
-  notifies :run, "windows_batch[move_drupal]"
+  notifies :run, "windows_batch[move_drupal]", :immediately
   #not_if {::File.exists?(source_index_file)}
 end
 
 windows_batch "move_drupal" do
-  action :nothing
+  #action :nothing
   code <<-EOH
-  xcopy #{sourcepath}\\#{node['drupal']['windows']['source']['url'].split("/").last[0,21]} #{node['drupal']['windows']['path']} /e /y
+  xcopy #{sourcepath.gsub!('/','\\')}\\#{node['drupal']['windows']['source']['url'].split("/").last[0,21]} #{node['drupal']['windows']['path']} /e /y
   EOH
 end
 
-# create drupal database and user
-# Create Wordpress database
-# should check if this stuff exists and create if it doesn't
-#
-#windows_batch "create_drupal_database" do
-#  creates "c:/drupaldb.log"
-#  code <<-EOH
-#  cd c:/mysql/bin
-#  mysql -u root -e "CREATE DATABASE #{node['drupal']['db']['database']}"
-#  mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, INDEX,
-#  DROP, CREATE TEMPORARY TABLES, LOCK TABLES
-#  ON #{node['drupal']['db']['database']}.*
-#  TO '#{node['drupal']['db']['user']}'@'#{node['drupal']['db']['host']}' IDENTIFIED BY 'opscode';"
-#  mysql -u root -e "FLUSH PRIVILEGES"
-#  touch c:/drupaldb.log
-#  EOH
-#end
-#
-
-
-#
+###########################################################
 # Azure storage plugin
-#
-sourcepath=::File.join(Chef::Config[:file_cache_path], "azure-storage")
-distfilename = "azure-storage-latest.zip"
-distzipfile = ::File.join(sourcepath, distfilename)
+sourcepath="#{Chef::Config[:file_cache_path]}/azure-storage"
+distzipfile = "#{sourcepath}/azure-storage-latest.zip"
 
 # source directory where we land and unroll our zip
 directory sourcepath do
@@ -95,29 +80,27 @@ end
 remote_file distzipfile do
   source node['drupal']['windows']['azure_storage']['source']
   checksum node['drupal']['windows']['azure_storage']['checksum']
-  notifies :unzip, "windows_zipfile[#{sourcepath}]"
+  notifies :unzip, "windows_zipfile[#{sourcepath}]", :immediately
 end
 
 windows_zipfile sourcepath do
   action :nothing
   source distzipfile
-  notifies :run, "windows_batch[move_azure_storage]"
+  notifies :run, "windows_batch[move_azure_storage]", :immediately
 end
 
 windows_batch "move_azure_storage" do
-  action :nothing
+  #action :nothing
   code <<-EOH
-  xcopy #{sourcepath}\\azure #{node['drupal']['windows']['path']} /e /y
+  xcopy #{sourcepath.gsub!('/','\\')}\\azure #{node['drupal']['windows']['modules']['path']}\\azure /i /y
   EOH
 end
 
 
-#
+###########################################################
 # Azure ACS plugin
-#
-sourcepath=::File.join(Chef::Config[:file_cache_path], "acs-latest")
-distfilename = "azure-acs-latest.zip"
-distzipfile = ::File.join(Chef::Config[:file_cache_path],distfilename)
+sourcepath="#{Chef::Config[:file_cache_path]}/acs-latest"
+distzipfile = "#{sourcepath}/azure-acs-latest.zip"
 
 # source directory where we land and unroll our zip
 directory sourcepath do
@@ -128,19 +111,121 @@ directory sourcepath do
 remote_file distzipfile do
   source node['drupal']['windows']['azure_acs']['source']
   checksum node['drupal']['windows']['azure_acs']['checksum']
-  notifies :unzip, "windows_zipfile[#{sourcepath}]"
+  notifies :unzip, "windows_zipfile[#{sourcepath}]", :immediately
 end
 
 windows_zipfile sourcepath do
   action :nothing
   source distzipfile
-  notifies :run, "windows_batch[move_azure_acs]"
+  notifies :run, "windows_batch[move_azure_acs]", :immediately
 end
 
 windows_batch "move_azure_acs" do
+  #action :nothing
   code <<-EOH
-  xcopy #{sourcepath}\\azure #{node['drupal']['windows']['path']} /e /y
+  xcopy #{sourcepath.gsub!('/','\\')}\\azure_acs #{node['drupal']['windows']['modules']['path']}\\azure_acs /i /y
   EOH
-  # don't copy if 1) file exists or 2) file in unrolled zip file doesn't match hash
 end
+
+###########################################################
+#  sql php drivers
+sourcepath="#{Chef::Config[:file_cache_path]}/drupal-sqlserv-driver"
+distzipexe = "#{sourcepath}/drupal-sqlserv-driver.exe"
+
+# source directory where we land and unroll our zip
+directory sourcepath do
+  action :create
+  not_if {::File.directory?(sourcepath)}
+end
+
+remote_file distzipexe do
+  source node['drupal']['windows']['sqlserv-driver']['source']
+  checksum node['drupal']['windows']['sqlserv-driver']['checksum']
+  #notifies :unzip, "windows_zipfile[#{sourcepath}]"
+  notifies :run, 'windows_batch[open-sqlserv-driver]', :immediately
+end
+
+# unroll and dump in same dir
+windows_batch "open-sqlserv-driver" do
+  action :nothing
+  code <<-EOH
+  #{distzipexe} /T:#{sourcepath.gsub!('/','\\')} /C /Q
+  EOH
+  notifies :run, 'windows_batch[move_sqlserv-plugin]'
+end
+
+#
+# TODO -- install
+# ref: http://www.php.net/manual/en/sqlsrv.installation.php
+# ref: http://drupal.org/node/1207972
+# The SQLSRV extension is enabled by adding appropriate DLL file to
+# your PHP extension directory and the corresponding entry to the php.ini file
+# copy JUST the one we want over
+windows_batch "move_sqlserv-plugin" do
+  #action :nothing
+  #xcopy #{sourcepath.gsub!('/','\\')}\\#{node['drupal']['windows']['database']['sqlserver']['driver']} #{node['drupal']['php']['install_path']}\\ext /y
+  code <<-EOH
+  xcopy #{sourcepath}\\#{node['drupal']['windows']['database']['sqlserver']['driver']} #{node['drupal']['php']['install_path']}\\ext /y
+  EOH
+  notifies :create, 'ruby_block[insert_line_phpini]'
+end
+
+# append to
+# #{node['drupal']['php']['install_path']}\\php.ini
+# the line
+# extension=#{node['drupal']['windows']['database']['sqlserver']['driver']}
+#
+ruby_block "insert_line_phpini" do
+  block do
+    file = Chef::Util::FileEdit.new("#{node['drupal']['php']['install_path']}\\php.ini")
+    file.insert_line_if_no_match("/extension=#{node['drupal']['windows']['database']['sqlserver']['driver']}/", "extension=#{node['drupal']['windows']['database']['sqlserver']['driver']}")
+    file.write_file
+  end
+end
+
+############################################################
+##sqlserv-plugin
+#sourcepath="#{Chef::Config[:file_cache_path]}/drupal-sqlserv-plugin"
+#distzipfile = "#{sourcepath}/drupal-sqlserv-plugin.zip"
+#
+## source directory where we land and unroll our zip
+#directory sourcepath do
+#  action :create
+#  not_if {::File.directory?(sourcepath)}
+#  end
+#
+#remote_file distzipfile do
+#  source node['drupal']['windows']['sqlserv-plugin']['source']
+#  checksum node['drupal']['windows']['sqlserv-plugin']['checksum']
+#  notifies :unzip, "windows_zipfile[#{sourcepath}]", :immediately
+#end
+#
+#windows_zipfile sourcepath do
+#  action :nothing
+#  source distzipfile
+#  notifies :run, "windows_batch[move_sqlserv-plugin]", :immediately
+#end
+#
+#windows_batch "move_sqlserv-plugin" do
+#  #action :nothing
+#  code <<-EOH
+#  xcopy #{sourcepath.gsub!('/','\\')}\\sqlsrv #{node['drupal']['windows']['modules']['path']}\\sqlsrv /i /y
+#  EOH
+#end
+#
+
+#
+# install drush for windows
+# to get some command line goodness
+#
+# http://drush.ws/sites/default/files/attachments/Drush-5.8-2012-12-10-Installer-v1.0.20.msi
+#
+
+# Delete the stuff
+directory sourcepath do
+  recursive true
+  action :delete
+end
+
+
 
