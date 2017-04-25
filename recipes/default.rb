b02aada4d2c085048825d695b3f7d3a14aa66bd4
Tim Smith <tsmith@chef.io>
2017-04-24 23:26:28 -0700

    Remove node.set usage
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,7 @@ include_recipe 'php-windows'
 include_recipe 'vcruntime::vc10'
 include_recipe 'drupal-windows::php-sqlserver-drivers'
 
-node.set_unless['drupal']['database']['prefix'] = (0...6).map{('a'..'z').to_a[rand(26)]}.join << '_'
+node.normal_unless['drupal']['database']['prefix'] = (0...6).map{('a'..'z').to_a[rand(26)]}.join << '_'
 
 ###########################################################
 # TODO
@@ -243,5 +243,3 @@ end
 service 'Apache2.2' do
   action :restart
 end
-
-
